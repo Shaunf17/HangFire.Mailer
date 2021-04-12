4fd08c0cd2dde69b8f553e7b1f66cd4c8d115273
Shaun <shaunf1996@hotmail.co.uk>
2021-04-12 11:15:39 +0100

    Add project files.

---
+++ HangFire.Mailer/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(HangFire.Mailer.Startup))]
+
+namespace HangFire.Mailer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration
+             .UseSqlServerStorage(
+                 "MailerDb",
+                 new SqlServerStorageOptions { QueuePollInterval = TimeSpan.FromSeconds(1) });
+
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
